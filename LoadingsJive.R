6df9301a6314ab6a34b7740df908a67d7574a803
ericaponzi <52282039+ericaponzi@users.noreply.github.com>
2020-02-17 12:01:15 +0100

    Add files via upload

--- LoadingsJive.R
+++ LoadingsJive.R
@@ -4,14 +4,11 @@
 # k is the number of variables we want to extract
 
 
-extract_loadings_jive <- function(jiveResults, k = 10){
-  
-  # extract joint loadings
+extract_loadings_jive <- function(data, jiveResults, k = 10){
   n_joint <- jiveResults$rankJ
   SVD = svd(do.call(rbind,jiveResults$joint), nu=n_joint, nv=n_joint)
   joint.load <- SVD$u
   
-  # extract individual loadings for each source
   indiv.load <- list()
   n_indiv <- list()
   for (i in 1:length(jiveResults$rankA)){
@@ -19,31 +16,26 @@ extract_loadings_jive <- function(jiveResults, k = 10){
     SVDI = svd(jiveResults$individual[[i]],nu=n_indiv[[i]],nv=n_indiv[[i]])
     indiv.load[[i]] <- SVDI$u
   }
-  
   loadings <- list(joint.load, indiv.load)
   names(loadings) <- c('Joint', 'Indiv')
-  
   # visualize variables with higher importance in joint
   abs.loadings.joint <- abs(joint.load)
   topload.joint <- order(abs.loadings.joint, decreasing = TRUE)
-  varnames <- c(rownames(dataNOWAC[[1]]), rownames(dataNOWAC[[2]]), 
-                rownames(dataNOWAC[[3]]))
+  varnames <- unlist(lapply(data, rownames))
   top.joint.var <- varnames[topload.joint[1:k]]
   
   # visualize variables with higher importance in individual
   abs.loadings.indiv <- lapply(indiv.load, abs)
   topload.indiv <- list()
   top.indiv.var <- list()
-  for (i in 1:length(dataNOWAC)){
+  for (i in 1:length(jiveResults$rankA)){
     topload.indiv[[i]] <- apply(abs.loadings.indiv[[i]],2, 
                                 function(x) order(x, decreasing = TRUE))
     
-    top.indiv.var[[i]] <- rownames(dataNOWAC[[i]])[topload.indiv[[i]][1:k]]
+    top.indiv.var[[i]] <- rownames(data[[i]])[topload.indiv[[i]][1:k]]
   }
-  
   top.var <- list(top.joint.var, top.indiv.var)
   names(top.var) <- c('Joint', 'Indiv')
-  
   res <- list(loadings, top.var)
   names(res) <- c('Loadings', 'TopVariables')
   return(res)
