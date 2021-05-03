a2f702389dbfb14c0713e18f39c0359f5723d8ec
Erica Ponzi <ericapo@uio.no>
2021-05-03 10:51:53 +0200

    Update application_TCGA.R

--- TCGAapplication/application_TCGA.R
+++ TCGAapplication/application_TCGA.R
@@ -232,42 +232,26 @@ data.logistic$Y <- as.factor(data.logistic$Y)
 xvars <- names(data.logistic[2:(rj+r1+r2+r3+1)])
 formula <- paste( 'Y', '~', paste( xvars, collapse=' + ' ) )
 
-xvars.j <- names(data.logistic[2:(rj+1)])
-formula.j <- paste( 'Y', '~', paste( xvars.j, collapse=' + ' ) )
-
-xvars.i <- names(data.logistic[(rj+2):((rj+r1+r2+r3+1))])
-formula.i <- paste( 'Y', '~', paste( xvars.i, collapse=' + ' ) )
 
 xvars.ni <- names(data.logistic[(rj+r1+r2+r3+2):ncol(data.logistic)])
 formula.ni <- paste( 'Y', '~', paste( xvars.ni, collapse=' + ' ) )
 
-xvars.ni.m <- names(data.logistic[(rj+r1+r2+r3+2): (rj+r1+r2+r3+6)])
-formula.ni.m <- paste( 'Y', '~', paste( xvars.ni.m, collapse=' + ' ) )
+
 
 #index <- sample(150, 100)
 data.train <- data.logistic
 model.fit <- multinom(formula , 
                  data = data.train)
-model.fit.j <- multinom(formula.j, 
-                   data = data.train)
-model.fit.i <- multinom(formula.i, 
-                   data = data.train)
+
 model.fit.ni <- multinom(formula.ni, 
                     data = data.train)
-model.fit.ni.m <- multinom(formula.ni.m, 
-                         data = data.train)
 
 
 data.test <- data.logistic
 data.test$pred <- predict(model.fit, data.test, type = 'class')
-data.test$pred.j <- predict(model.fit.j, data.test, type = 'probs')
-data.test$pred.i <- predict(model.fit.i, data.test, type = 'probs')
 data.test$pred.ni <- predict(model.fit.ni, data.test, type = 'class')
-data.test$pred.ni.m <- predict(model.fit.ni.m, data.test, type = 'class')
 
 multiclass.roc(data.test$Y, as.numeric(data.test$pred))$auc
-multiclass.roc(data.test$Y, as.numeric(data.test$pred.j))$auc
-multiclass.roc(data.test$Y, as.numeric(data.test$pred.i))$auc
 multiclass.roc(data.test$Y, as.numeric(data.test$pred.ni))$auc
 
 
@@ -284,15 +268,10 @@ folds <- cut(seq(1, nrow(data.logistic)), breaks = 10, labels = FALSE)
 
 
 auc <- c()
-auc.j <- c()
-auc.i <- c()
 auc.ni <- c()
-auc.ni.m <- c()
 roc <- list()
 roc.ni <- list()
-roc.j <- list()
-roc.i <- list()
-roc.ni.m <- list()
+
 
 for (i in 1:10){
   testIndexes <- which(folds == i, arr.ind = TRUE)
@@ -302,40 +281,27 @@ for (i in 1:10){
                    data = data.train)
   model.fit.ni <- multinom(formula.ni, 
                     data = data.train)
-  model.fit.j <- multinom(formula.j, 
-                     data = data.train)
-  model.fit.i <- multinom(formula.i, 
-                     data = data.train)
-  model.fit.ni.m <- multinom(formula.ni.m, 
-                          data = data.train)
+  
   
   
   data.test$pred <- predict(model.fit, data.test, type = 'class')
   data.test$pred.ni <- predict(model.fit.ni, data.test, type = 'class')
-  data.test$pred.j <- predict(model.fit.j, data.test, type = 'class')
-  data.test$pred.i <- predict(model.fit.i, data.test, type = 'class')
-  data.test$pred.ni.m <- predict(model.fit.ni.m, data.test, type = 'class')
+  
   
   roc[[i]] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred))
   roc.ni[[i]] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.ni))
-  roc.j[[i]] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.j))
-  roc.i[[i]] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.i))
-  roc.ni.m[[i]] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.ni.m))
+  
   
   
   auc[i] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred))$auc
   auc.ni[i] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.ni))$auc
-  auc.ni.m[i] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.ni.m))$auc
-  auc.j[i] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.j))$auc
-  auc.i[i] <- multiclass.roc(data.test$Y, as.numeric(data.test$pred.i))$auc
+  
  
   
 }
 mean(auc)
 mean(auc.ni)
-mean(auc.ni.m)
-mean(auc.j)
-mean(auc.i)
+
 
 library(randomForest)
 data.rf <- data.logistic[, 1:(rj+r1+r2+r3+1)]
@@ -355,38 +321,6 @@ print(rf)
 varImpPlot(rf, sort = TRUE, n.var = 10)
 
 
-
-imp <- importance(rf)
-ord <- order(imp, decreasing = TRUE)[1:10]
-# 1 = caco
-# 2, 3, 4, 5 = joint
-# 6, 7, 8, 9, 10 = Ind mRNA
-# 11, 12, 13, 14, 15 = Ind miRNA
-# 16 = Ind met
-
-shape <- rep('darkgrey', 10)
-shape[c(1, 2, 3, 4)] <- 'blue'
-
-varimp <- data.frame(imp = imp[ord], var = c('Joint2', 'Joint1', 'Joint3', 'Joint4', 
-                                             'mRNAInd4', 'mRNAInd1', 'MetInd1', 
-                                              'miRNAInd1', 'miRNAInd3',
-                                             'mRNAInd5'), shape = shape)
-
-varimp <- varimp[order(varimp$imp), ]
-
-
-
-qplot(x = imp, 
-      y = reorder(var, imp), color = factor(shape), data = varimp, geom = "point") +  geom_point(size = 2) +
-  xlab("MeanDecreaseGini") + ylab('') + theme(legend.position = "none") +
-  ggtitle('Variable Importance') + geom_segment(aes(x = 0, y = var, xend = imp, yend = var ), lty = 2)
-
-
-# save all results 
-save(ajiveResults, file = 'ajiveResultsTCGA.RData')
-l <- list(data.logistic, folds)
-save(l, file = 'CV_TCGA.RData')
-save(rf, file = 'RF_TCGA.RData')
 
 
 
@@ -409,27 +343,6 @@ varImpPlot(rf, sort = TRUE, n.var = 10)
 multiclass.roc(test.data$Y, as.numeric(predictions))
 
 
-rf.i <- randomForest(Y~., data = train.data[, c('Y', xvars.i)], 
-                     ntree = 1000, proximity = TRUE, 
-                     na.action = na.omit)
-predictions.i <- predict(rf.i, newdata = test.data[, c('Y', xvars.i)])
-train.data <- na.omit(train.data)
-table(predict(rf.i), train.data$Y)
-cm.i <- table(predictions.i, test.data$Y)
-accuracy.i = sum(diag(cm.i))/sum(cm.i)
-multiclass.roc(test.data$Y, as.numeric(predictions.i))
-print(rf.i)
-
-rf.j <- randomForest(Y~., data = train.data[, c('Y', xvars.j)], 
-                     ntree = 1000, proximity = TRUE, 
-                     na.action = na.omit)
-predictions.j <- predict(rf.j, newdata = test.data[, c('Y', xvars.j)])
-train.data <- na.omit(train.data)
-table(predict(rf.j), train.data$Y)
-cm.j <- table(predictions.j, test.data$Y)
-accuracy.j = sum(diag(cm.j))/sum(cm.j)
-multiclass.roc(test.data$Y, as.numeric(predictions.j))
-print(rf.j)
 
 rf.ni <- randomForest(Y~., data = train.data[, c('Y', xvars.ni)], 
                      ntree = 1000, proximity = TRUE, 
@@ -447,90 +360,3 @@ rf.list <- list(rf, rf.j, rf.i, rf.ni)
 names(rf.list) <- c('rf', 'rf.j', 'rf.i', 'rf.ni')
 save(rf.list, file = 'RFlistTCGA.RData')
 
-# check impact of mRNA on non-integrative models
-data.ni <- as.data.frame(cbind(Y, mrna.pcs, mirna.pcs, met.pcs))
-names(data.ni) <- c('Y', 'PC1mr', 'PC2mr', 'PC3mr', 'PC4mr', 'PC5mr',
-                    'PC1mi', 'PC2mi', 'PC3mi', 'PC4mi', 'PC5mi',
-                    'PC1met', 'PC2met')
-
-xvars.ni <- names(data.ni[2:ncol(data.ni)])
-formula.ni <- paste( 'Y', '~', paste( xvars.ni, collapse=' + ' ) )
-
-model.fit.ni <- multinom(formula.ni, 
-                         data = data.ni)
-
-summary(model.fit.ni)
-
-xvars.ni.m <- names(data.ni[2:6])
-formula.ni.m <- paste( 'Y', '~', paste( xvars.ni.m, collapse=' + ' ) )
-
-model.fit.ni.m <- multinom(formula.ni.m, 
-                         data = data.ni)
-
-summary(model.fit.ni.m)
-
-xvars.ni.mi <- names(data.ni[7:11])
-formula.ni.mi <- paste( 'Y', '~', paste( xvars.ni.mi, collapse=' + ' ) )
-
-model.fit.ni.mi <- multinom(formula.ni.mi, 
-                           data = data.ni)
-
-summary(model.fit.ni.mi)
-
-xvars.ni.me <- names(data.ni[12:13])
-formula.ni.me <- paste( 'Y', '~', paste( xvars.ni.me, collapse=' + ' ) )
-
-model.fit.ni.me <- multinom(formula.ni.me, 
-                            data = data.ni)
-
-summary(model.fit.ni.me)
-
-
-data.ni$pred.ni <- predict(model.fit.ni, data.ni, type = 'prob')
-multiclass.roc(data.ni$Y, as.numeric(data.ni$pred.ni))$auc
-data.ni$pred.ni.m <- predict(model.fit.ni.m, data.ni, type = 'prob')
-multiclass.roc(data.ni$Y, as.numeric(data.ni$pred.ni.m))$auc
-data.ni$pred.ni.mi <- predict(model.fit.ni.mi, data.ni, type = 'prob')
-multiclass.roc(data.ni$Y, as.numeric(data.ni$pred.ni.mi))$auc
-data.ni$pred.ni.me <- predict(model.fit.ni.me, data.ni, type = 'prob')
-multiclass.roc(data.ni$Y, as.numeric(data.ni$pred.ni.me))$auc
-
-# lasso 
-X.full <- cbind(met, mRNA, miRNA)
-p <- ncol(X.full)
-n <- nrow(X.full)
-B=50
-
-# needs p+1 for intercept
-#beta.lasso <- matrix(NA, ncol=2*B, nrow=p+1)
-auc.lasso <- c()
-
-for(j in 1:B){
-  
-  #set.seed(j*10)
-  ss=sample.int(n,n/2)
-  Y1=Y[ss]
-  X1=X.full[ss,]
-  Y2=Y[-ss]
-  X2=X.full[-ss,]
-  
-  lambda_seq <- 10^seq(2, -2, length.out = 100)
-  
-  
-  cv <- cv.glmnet(X1, Y1, alpha = 1, family = 'multinomial', lambda = lambda_seq)
-  lambda.best <- cv$lambda.min
-  fit.lasso <- glmnet(X1, Y1, alpha = 1,   family = 'multinomial', 
-                      lambda = lambda.best, standardize = TRUE)
-  
-  
-  predictions <- predict(fit.lasso, X2, type = 'class')
-  predictions <- as.factor(predictions)
-  
-  auc.lasso[j] <- as.numeric(multiclass.roc(Y2, as.numeric(predictions))$auc)
-  
-  
-  
-  
-  
-}
-mean(auc.lasso) #0.68 with 2/3 of the data, 0. with 1/2 0.647
